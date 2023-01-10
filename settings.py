b560bceacf7da0c981a3f8eaf02aca8d6bf2d5a2
Ka-tusha <116356216+Ka-tusha@users.noreply.github.com>
2023-01-10 14:43:16 +0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = "C:\chromedriver\chromedriver.exe"
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'super.presentashion@yandex.ru'
+    valid_password = 'Rostelecom721'
+    valid_email_for_reg = 'super.presentashion@yandex.ru'
+    invalid_email = 'rostelek@gmail.com'
+    invalid_password = 'Uid125777nklP89683'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Сергей'
+    l_name = 'Петров'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'к'
+    latin_generate_string = 'ljuyejf'
+    chinese_chars = '我不会说汉怎么样你会说英语'
+    special_chars = '!@#${}$*****%^&*(#)_+_+=//\[\}]]]'
+    numbers = 173
+    empty = ''
+    passw1 = 'Qw987654321qw'
+    passw2 = 'Uid125777nklP89683'
+    passw3 = 'IOdd9876543210ELo8964776'
